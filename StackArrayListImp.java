95c431b801286bcb9c1452e88226cd3272bc4921
mahfoozalamcse <148795525+mahfoozalamcse@users.noreply.github.com>
2025-01-20 05:28:33 +0000

    Linked list implementation of stack

---
+++ StackArrayListImp.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class StackArrayListImp {
+
+    // Dynamic stack implimentation using array array list 
+
+    public static class MyStack{
+       ArrayList<Integer> al = new ArrayList<>();
+
+    // add data
+    void push(int x){
+        al.add(x);
+    }
+
+     // pop
+    int pop(){
+        int res = al.get(al.size()-1);
+        al.remove(al.size()-1);
+        return res;
+    }
+
+    // peek
+    int peek(){
+        return al.get(al.size()-1);
+    }
+
+    // size
+    int size(){
+        return al.size();
+    }
+
+    // check stack is empty
+    boolean  isEmpty(){
+        return al.isEmpty();
+    }
+    
+    }
+    public static void main(String[] args) {
+      MyStack s = new MyStack();
+      
+    }
+}
